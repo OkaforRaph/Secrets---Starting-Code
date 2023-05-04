32ba32bb9826d819ea99efdd5846de8f459890dc
Okafor Raphael <95545345+OkaforRaph@users.noreply.github.com>
2023-05-04 17:09:07 +0100

    Added enviroment files to my secerts website

---
+++ app.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const { Console } = require("console");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+	useNewUrlParser: true,
+});
+
+const userSchema = new mongoose.Schema({
+	email: "String",
+	password: "String",
+});
+
+userSchema.plugin(encrypt, {
+	secret: process.env.SECRET,
+	encryptedFields: ["password"],
+});
+
+const User = new mongoose.model("User", userSchema);
+
+// Home route
+app.get("/", function (req, res) {
+	res.render("home");
+});
+
+// Login route
+app
+	.route("/login")
+	.get(function (req, res) {
+		res.render("login");
+	})
+	.post(function (req, res) {
+		const username = req.body.username;
+		const password = req.body.password;
+
+		// Find the user with that username from the database
+		User.findOne({ email: username })
+			.then(function (foundUser) {
+				if (foundUser.password === password) {
+					res.render("secrets");
+				} else {
+					res.send("Wrong Password!");
+				}
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
+
+// Register route
+app
+	.route("/register")
+	.get(function (req, res) {
+		res.render("register");
+	})
+	.post(function (req, res) {
+		const newUser = new User({
+			email: req.body.username,
+			password: req.body.password,
+		});
+
+		// Save the new user, and then render the secrets page if sucessful
+		newUser
+			.save()
+			.then(function (prams) {
+				res.render("secrets");
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	});
+
+// Listening on port 3000
+app.listen(3000, function () {
+	console.log("Server is started on port 3000.");
+});
